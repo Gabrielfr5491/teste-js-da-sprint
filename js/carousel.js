154e4d8f6fcc5dcf12edf321d2215f0d8545d314
GABRIEL FREIRE DE SANTANA <026.067733@universidadesenaicimatec.edu.br>
2026-05-12 11:52:07 -0300

    versao

--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,7 @@
 let carouselArr = [];
 
 class Carousel {
-    
+
     constructor(image, title, link) {
         this.image = image;
         this.title = title;
@@ -12,16 +12,16 @@ class Carousel {
     static Start(arr) {
         if (arr) {
             if (arr.length > 0) {
-         
-                Carousel._items = arr; 
+
+                Carousel._items = arr;
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
-                
-                Carousel.Next(); 
-                
-               
-                Carousel._interval = setInterval(function() { 
-                    Carousel.Next(); 
+
+                Carousel.Next();
+
+
+                Carousel._interval = setInterval(function () {
+                    Carousel.Next();
                 }, 2000);
             }
         } else {
@@ -30,7 +30,7 @@ class Carousel {
     }
 
     static Next() {
-    
+
         const container = document.getElementById("carousel");
         const titleContainer = document.getElementById("carousel-title");
 
@@ -38,18 +38,33 @@ class Carousel {
 
         if (container && titleContainer) {
             container.innerHTML = `
-                <a href="${item.link}">
-                    <button id = "esquerda"><</button>
-                        <img src="img/${item.image}" style="width:50%; transition: opacity 0.5s ease-in-out ;">
-                    <button id="direita">></button>
-                </a>`;
+                    <button id = "esquerda" type="button"><</button>
+                        <img src="img/${item.image}";">
+                    <button id="direita">></button>`;
             titleContainer.innerHTML = `<p>${item.title}</p>`;
         }
 
-        Carousel._sequence++;
-        if (Carousel._sequence >= Carousel._size) {
-            Carousel._sequence = 0; 
-        }    
+        const button_eq = document.getElementById("esquerda");
+
+        button_eq.onclick = () => {
+            Carousel._sequence--;
+
+            if (Carousel._sequence < 0) Carousel._sequence = Carousel._size - 1;
+
+            Carousel.Next();
+        }
+
+        document.getElementById("direita").onclick = () => {
+            Carousel._sequence++;
+            if (Carousel._sequence >= Carousel._size) {
+                Carousel._sequence = 0;
+            }
+
+            Carousel.Next();
+        }
+
+        Carousel._sequence++
+        Carousel.Next
     }
 
 }
